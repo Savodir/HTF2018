8649827ba9f2736b7b66c2147bc917a0dddb9609
Carl Vancluysen <Carl.Vancluysen@gmail.com>
2018-11-29 13:05:03 +0100

    Challenge04

---
+++ HTF/HTF/Challenge04.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTF
+{
+    class Challenge04: Challenge
+    {
+        List<int> primesList = new List<int>();
+        int min, max;
+        public Challenge04(int min, int max)
+        {
+            this.max = max;
+            this.min = min;
+            crack();
+        }
+
+        public override void crack()
+        {
+                List<int> primes = new List<int>();
+                long sum = 0;
+                long n = max;
+                bool[] e = new bool[n];//by default they're all false
+                for (int i = 2; i < n; i++)
+                {
+                    e[i] = true;//set all numbers to true
+                }
+                //weed out the non primes by finding mutiples 
+                for (int j = 2; j < n; j++)
+                {
+                    if (e[j])//is true
+                    {
+                        for (long p = 2; (p * j) < n; p++)
+                        {
+                            e[p * j] = false;
+                        }
+                        if (j > min)
+                        {
+                            primesList.Add(j);
+                            Trace.WriteLine(j);
+                        }
+                    }
+                }
+        }
+
+        public override void get()
+        {
+            throw new NotImplementedException();
+        }
+        public override void post()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+}
--- HTF/HTF/MainWindow.xaml.cs
+++ HTF/HTF/MainWindow.xaml.cs
@@ -36,7 +36,7 @@ namespace HTF
        //     Thread.Sleep(11000); //ms
             postManager.postChallenge("593bc0a2e0dfdc53b239bc2a96ab0fd5", identifier, "sum", "183", "18274857-ff06-4535-8352-fa9d67e122e0");
             //  Console.WriteLine(response);
-            Challenge03 challenge03 = new Challenge03(500, 20000);
+            Challenge04 challenge04 = new Challenge04(500, 20000);
             List<int> primes = challenge03.getPrimesList();
         }
     }
