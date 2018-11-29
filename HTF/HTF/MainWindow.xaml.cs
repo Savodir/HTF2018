fd7801bb7280b0998d986044ba56fe665b91c475
Carl Vancluysen <Carl.Vancluysen@gmail.com>
2018-11-29 12:25:06 +0100

    Challenge3

--- HTF/HTF/MainWindow.xaml.cs
+++ HTF/HTF/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -21,8 +22,11 @@ namespace HTF
     /// </summary>
     public partial class MainWindow : Window
     {
+        
         private String identifier = "MjAzMzQ5ZTAtYzY2MC00ZjI3LThmYzItMDY1MjY3MTE0ODYx";
         List<String> challengeCodes;
+        bool flag;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -32,8 +36,8 @@ namespace HTF
        //     Thread.Sleep(11000); //ms
             postManager.postChallenge("593bc0a2e0dfdc53b239bc2a96ab0fd5", identifier, "sum", "183", "18274857-ff06-4535-8352-fa9d67e122e0");
             //  Console.WriteLine(response);
-
-
+            Challenge03 challenge03 = new Challenge03(500, 20000);
+            List<int> primes = challenge03.getPrimesList();
         }
     }
 }
