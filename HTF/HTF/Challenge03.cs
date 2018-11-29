c677da1f91818b3ef434900476e88136d79ad81e
Jonas Luyks <jonas_luyks@hotmail.com>
2018-11-29 11:42:31 +0100

    Create Challenge03.cs

---
+++ HTF/HTF/Challenge03.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTF
+{
+    class Challenge03 : Challenge
+    {
+        public override void crack()
+        {
+            foreach (InputValue item in inputValues)
+            {
+
+            }
+        }
+
+        public override void get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void post()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool isLetter(char c)
+        {
+            return (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z');
+        }
+    }
+}
